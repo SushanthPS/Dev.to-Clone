42fecc090c6180e29bd726adcbfb5d5811f999fe
Sushanth PS <getsushanthps@gmail.com>
2021-10-30 20:45:03 +0530

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,11 @@
+import { Route, Switch } from "react-router-dom";
+
+export default function Routes() {
+    return (
+        <Switch>
+            <Route path="/" exact>
+                Homepage component
+            </Route>
+        </Switch>
+    );
+}
