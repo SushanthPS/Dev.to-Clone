2e5b5d4cce048a02b6c88b1ed70a265e340732f9
piyushrana00456 <54103082+piyushrana00456@users.noreply.github.com>
2021-11-13 12:08:42 +0530

    nothing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-// import Routes from "./routes/Routes";
+import Routes from "./routes/Routes";
 import "./App.css";
-import { Post } from "./components/Post/Post/Post";
 
 function App() {
   return (
     <div>
-      {/* <Routes /> */}
-      <Post />
+      <Routes />
     </div>
   );
 }
